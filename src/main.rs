1774e6af1672e32586f934759df4bfca47332a2f
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-12-12 22:57:44 +0900

    awaitを利用

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,15 @@ async fn hello_world() {
     println!("hello world")
 }
 
+async fn hello_world2() {
+    println!("hello world2")
+}
+
+async fn run() {
+    hello_world().await;
+    hello_world2().await;
+}
+
 fn main() {
-    let future = hello_world();
-    block_on(future);
+    block_on(run())
 }
